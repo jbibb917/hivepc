bf976782c5ed566f6f6a7d1940a260af8020844a
James <jbibb917@gmail.com>
2017-08-21 22:18:52 +0100

    changed design, started work on the routing.

--- js/common.js
+++ js/common.js
@@ -1,5 +1,24 @@
-import Typed from '../bower_components/typed.js/src/typed.js';
+var myApp = angular.module("myApp", ['ngRoute','RouteControllers']);
 
-  var typed = new Typed('.element', {
-    stringsElement: '#typed-strings'
-  });
+angular.module('myApp').config(function($locationProvider, $routeProvider) {
+    $locationProvider.html5Mode(true);
+	
+	$routeProvider
+	.when('/',{
+		 templateUrl:'templates/about.html',
+		  controller:'AboutController',
+		})
+	.when('/build',{
+		 templateUrl:'templates/build.html',
+		  controller:'BuildController',
+		})
+	.when('/contact',{
+		 templateUrl:'templates/contact.html',
+		  controller:'ContactController',
+		})
+	.when('/inside',{
+		 templateUrl:'templates/inside.html',
+		  controller:'InsideController',
+		});
+	
+});
