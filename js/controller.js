8c4dd3b9e72f89e6d6f26121d64bb607df5ae4f7
James <jbibb917@gmail.com>
2017-09-28 07:36:33 +0100

    added angular injection for the review cards. Added email field to the contact form#

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,4 @@
-app.controller('contactController',
-				 
-	function($scope){
+app.controller('contactController', function($scope){
 	
 	$scope.submitForm = function()
 	{
@@ -9,3 +7,60 @@ app.controller('contactController',
 	}	
 	};
 });
+
+app.controller('reviewController', ['$scope', function($scope){
+
+	$scope.reviews = [
+	{
+	name: 'John Smith',
+	overview: 'Some quick example text to build on the card title and make up the bulk of the cards content.',
+	GPU:'Nvidia GeForce 1070',
+	CPU:'Intel i7',
+	RAM:'16gb',
+	Cooler:'Coolermaster Seidon v2',
+	Case:'NZXT h440',
+	cover:'../images/caspar-rubin-220711.jpg'
+	},
+	{
+	name: 'Paul Harrison',
+	overview: 'Some quick example text to build on the card title and make up the bulk of the cards content.',
+	GPU:'Nvidia GeForce 980ti',
+	CPU:'Intel i5',
+	RAM:'16gb',
+	Cooler:'Coolermaster Seidon v2',
+	Case:'NZXT h440',
+	cover:'../images/caspar-rubin-220711.jpg'
+	},
+	{
+	name: 'Paul Harrison',
+	overview: 'Some quick example text to build on the card title and make up the bulk of the cards content.',
+	GPU:'Nvidia GeForce 980ti',
+	CPU:'Intel i5',
+	RAM:'16gb',
+	Cooler:'Coolermaster Seidon v2',
+	Case:'NZXT h440',
+	cover:'../images/caspar-rubin-220711.jpg'
+	},	
+	{
+	name: 'Super Duper',
+	overview: 'Some quick example text to build on the card title and make up the bulk of the cards content.',
+	GPU:'Nvidia GeForce 980ti',
+	CPU:'Intel i5',
+	RAM:'16gb',
+	Cooler:'Coolermaster Seidon v2',
+	Case:'NZXT h440',
+	cover:'../images/caspar-rubin-220711.jpg'
+	},	
+	{
+	name: 'Grag Davis',
+	overview: 'Some quick example text to build on the card title and make up the bulk of the cards content.',
+	GPU:'Nvidia GeForce 980ti',
+	CPU:'Intel i5',
+	RAM:'16gb',
+	Cooler:'Coolermaster Seidon v2',
+	Case:'NZXT h440',
+	cover:'../images/caspar-rubin-220711.jpg'
+	},	
+	
+	];	
+}]);
