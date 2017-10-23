3735057a91eed1d8e19b2939ef8a5ca2dec60e7e
James <jbibb917@gmail.com>
2017-10-23 22:20:42 +0100

    fixed nav bar broken everything else

--- js/common.js
+++ js/common.js
@@ -1,28 +1,27 @@
-var app = angular.module('myApp', ['ngRoute']);
+var app = angular.module('myApp', ['ngRoute', 'ngMap']);
 	app.config(function($locationProvider, $routeProvider) {
 
     $locationProvider.html5Mode(true); //to eliminate the # in the url
 	
 	$routeProvider
-	.when('/',{
-		 templateUrl:'/templates/home.html',
+	.when("/",{
+		 templateUrl:"/templates/home.html",
 		})
-	.when('/build',{
-		 templateUrl:'/templates/build.html',
+	.when("/build",{
+		 templateUrl:"/templates/build.html",
 		})
-	.when('/contact',{
-		 templateUrl:'/templates/contact.html',
+	.when("/contact",{
+		 templateUrl:"/templates/contact.html",
 		})
-	.when('/reviews',{
-		 templateUrl:'/templates/reviews.html',
+	.when("/reviews",{
+		 templateUrl:"/templates/reviews.html",
 		})
-	.when('/about',{
-		 templateUrl:'/templates/about.html',
+	.when("/about",{
+		 templateUrl:"/templates/about.html",
 		})
 	.otherwise({
-		redirectTo: '/'
+		redirectTo: "/"
 		});
 		
-	angular.module('myApp', ['ngMap']);
 		
 });
