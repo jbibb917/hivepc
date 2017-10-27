dffcc0f395fa5dff138a5d22d0d6f33386bb0aa7
James <jbibb917@gmail.com>
2017-10-27 20:15:11 +0100

    got the news api working, added css to those areas, also jsutifed the text on the about page.

--- js/newsController.js
+++ js/newsController.js
@@ -1,6 +1,10 @@
-app.controller('newsController', function($scope, $http){
-	method: 'GET',
-	url: 'https://newsapi.org/v1/articles?source=polygon&sortBy=top&apiKey=750e3a33ee2a4bb88bfa754866c529d8'
-}).then(function(response){
-	$scope.news = response.data;
+app.controller('newscontroller', function($scope, $http) {
+    $http({
+        method : "GET",
+        url : "https://newsapi.org/v1/articles?source=polygon&sortBy=top&apiKey=750e3a33ee2a4bb88bfa754866c529d8"
+    }).then(function mySuccess(response) {
+        $scope.news = response.data;
+    }, function myError(response) {
+        $scope.news = response.statusText;
+    });
 });
